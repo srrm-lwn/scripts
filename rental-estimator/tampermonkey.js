66bd35f7a3b5df25d0a1bd6361200c3cd2d67a5e
sriram :zap <sriram.alwan@gmail.com>
2020-12-30 10:34:03 -0800

    Styling updates

--- rental-estimator/tampermonkey.js
+++ rental-estimator/tampermonkey.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Zillow Rental Estimate Summary
+// @name         Rental Estimate Summary
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://www.zillow.com/homes/*
+// @match        *://www.zillow.com/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
@@ -12,18 +12,25 @@
 (function() {
     'use strict';
 
+    if (window.location.href.indexOf("homedetails") != -1
+        || window.location.href.indexOf("community") != -1) {
+        console.log("On listing detail page. Skipping rental estimate summary.");
+        return;
+    }
+
     // Your code here...
-    console.log("### Starting Zillow Rental Estimate Summary");
+    console.info("### Starting Rental Estimate Summary");
 
     var articles = $("article");
-    console.log("Found " + articles.length + " articles");
+    console.info("Found " + articles.length + " articles");
 
+    var numOfEstimates = 0;
     articles.map(function() {
         var article = $(this);
         var url = article.find("div a").attr("href");
-        console.log("Fething URL - " + url);
+        console.debug("Fetching URL - " + url);
         $.get(url).then(function(data) {
-            console.log("Fetched URL - " + url);
+            console.debug("Fetched URL - " + url);
 
             var secret = $('<div id="secret"></div>');
             $('body').append(secret);
@@ -34,14 +41,17 @@
             var rentalEstimate = jQuery("#ds-rental-home-values").text().split("$")[1];
             secret.remove();
 
-            var estimate = rentalEstimate === undefined
-            ? "Rental Estimate is Not Available" :
-            "Rental Estimate: $" + rentalEstimate;
+            var estimateMsg = "Rental Estimate: Not Available";
+            if (rentalEstimate !== undefined) {
+                numOfEstimates++;
+                estimateMsg = "Rental Estimate: $" + rentalEstimate;
+            }
+            var cardInfo = article.find(".list-card-info");
+            var footer = $('<div></div>');
+            footer.css("order", "4");
+            footer.append('<div class="list-card-type"> ' + estimateMsg + ' </div>');
 
-            var footer = article.append('<div class="list-card-footer"></div>');
-            footer.append('<div class="list-card-type"> ' + estimate + ' </div>');
-
-            return article.append(footer);
+            return cardInfo.append(footer);
         });
     });
 })();
