e0b0a7db6b3f48e815c02dad42172e897ceea90d
sriram :zap <sriram.alwan@gmail.com>
2020-12-29 23:11:17 -0800

    Show rental estimate on tiles

--- rental-estimator/tampermonkey.js
+++ rental-estimator/tampermonkey.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://www.zillow.com/homes/for_sale*
+// @match        *://www.zillow.com/homes/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
@@ -15,33 +15,33 @@
     // Your code here...
     console.log("### Starting Zillow Rental Estimate Summary");
 
-    var urls = $("article div a").map(function() {
-        return $(this).attr("href");
-    }).get();
-    console.log("Retrieved " + urls.length + " URLs");
+    var articles = $("article");
+    console.log("Found " + articles.length + " articles");
 
-    var priceCards = $(".list-card-price");
-    console.log("Retrieved " + priceCards.length + " Price Cards");
-
-    var rentalEstimates = $('<div id="rentalEstimates"></div>');
-    var estimatesTable = $('<table style="border: 1px solid black;"></table>');
-    rentalEstimates.append(estimatesTable);
-    estimatesTable.append('<tr><th>URL</th><th>Monthly Estimate</th><th>Rental Estimate</th></tr>');
-    $('body').append(rentalEstimates);
-
-    urls.forEach(function(url) {
-        jQuery.get(url).then(function(data) {
+    articles.map(function() {
+        var article = $(this);
+        var url = article.find("div a").attr("href");
+        console.log("Fething URL - " + url);
+        $.get(url).then(function(data) {
             console.log("Fetched URL - " + url);
+
             var secret = $('<div id="secret"></div>');
             $('body').append(secret);
             secret.hide();
             var nodes = jQuery.parseHTML(data);
             secret.append(nodes);
-            var monthlyCost = jQuery('span:contains("Estimated monthly cost")').next().text();
+
             var rentalEstimate = jQuery("#ds-rental-home-values").text().split("$")[1];
             secret.remove();
 
-            estimatesTable.append('<tr><td>' + url + '</td><td>' + monthlyCost + '</td><td>' + rentalEstimate + '</td></tr>');
+            var estimate = rentalEstimate === undefined
+            ? "Rental Estimate is Not Available" :
+            "Rental Estimate: $" + rentalEstimate;
+
+            var footer = article.append('<div class="list-card-footer"></div>');
+            footer.append('<div class="list-card-type"> ' + estimate + ' </div>');
+
+            return article.append(footer);
         });
     });
 })();
